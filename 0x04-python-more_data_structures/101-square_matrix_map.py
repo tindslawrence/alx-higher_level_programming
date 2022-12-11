32e57c34722b1faa812e7cd8d17ee7494ee062a2
Lawrence Tindyebwa <tindslawrence@gmail.com>
2022-12-11 07:42:54 +0000

    Task 14

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,2 +1,3 @@
-quare_matrix_map(matrix=[]):
-    return (list(map(lambda x: list(map(lambda y: y ** 2, x[:])), matrix)))
+#!/usr/bin/python3
+def square_matrix_map(matrix=[]):
+    return list(map(lambda x: list(map(lambda y: y**2, x)), matrix[:]))
